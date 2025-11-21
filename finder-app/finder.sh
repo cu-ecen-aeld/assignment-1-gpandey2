13ff402abf0d8823fa6b8b9605a8ea1127449e4b
Gaurav <ultimatecannonbolt10@gmail.com>
2025-11-21 15:48:28 +0000

    Assignment 1

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -8,11 +8,11 @@ if [ $# -lt 2 ]; then
 	exit 1
 fi
 
-if [[ -d '${filesdir}' ]]; then
+if [[ ! -d ${filesdir} ]]; then
 	echo "Error: The mentioned directory ${filesdir} does not exist."
 	exit 1
 fi
 
-X=$(grep -arl ${searchstr} ${filesdir} | wc -l)
-Y=$(grep -ar ${searchstr} ${filesdir} | wc -l)
+X=$(grep -aRl ${searchstr} ${filesdir} | wc -l)
+Y=$(grep -aRr ${searchstr} ${filesdir} | wc -l)
 echo "The number of files are ${X} and the number of matching lines are ${Y}"
